70ba268325781a8e19a971eabbbff974182be914
De Ruvo Daniel <deruvodaniel@gmail.com>
2022-05-09 16:41:39 -0300

    fixed transitions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HomeView />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HomeView from "./components/HomeView.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HomeView,
   },
 };
 </script>
 
 <style lang="scss">
+// Global styles
+@import "@/scss/main.scss";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $fontMain;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
